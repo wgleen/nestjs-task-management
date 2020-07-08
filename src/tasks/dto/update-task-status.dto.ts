c5bb8a59f2ec5b6c3063a54e5a70ba4b693b38d2
Wallace Gleen <wallace.gleen@gmail.com>
2020-07-08 19:07:14 -0300

    add validation into params dto's

--- src/tasks/dto/update-task-status.dto.ts
+++ src/tasks/dto/update-task-status.dto.ts
@@ -1,5 +1,11 @@
+import {
+  IsNotEmpty,
+  IsEnum
+} from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class UpdateTaskStatusDto {
+  @IsNotEmpty()
+  @IsEnum(TaskStatus)
   status: TaskStatus;
 }
